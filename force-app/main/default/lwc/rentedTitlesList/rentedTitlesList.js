81b77157a895e7ee678a2b22188d0c2c8e190106
jreiner <jreiner@salesforce.com>
2021-07-08 08:53:13 -0400

    CurrentRentalsComponent
    2 apex classes 1 Test Class
    1 Lwc( 1 Js, 1 css, 1 html, 1 meta.xml)

---
+++ force-app/main/default/lwc/rentedTitlesList/rentedTitlesList.js
@@ -0,0 +1,44 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import getUserRentals from '@salesforce/apex/RentalController.getUserRentals'
+
+export default class RentedTitlesList extends LightningElement {
+    @track records;
+
+    connectedCallback(){
+        console.log('Connected.');
+
+        getUserRentals()
+            .then(result => {
+                console.log('Results - ', result[0]);
+                this.records = result;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+// one way
+//@wire (getMyRentals) rentals;
+
+// second way
+    // @wire(getUserRentals)
+    //     wiredRentals({
+    //         data, error
+    //     }){
+    //             //let today = new Date().toLocaleDateString()
+    //             console.log("DATA ===>> ",data);
+    //             console.log("ERROR ===>> ",error);
+    //         if(data){
+    //             this.records = data;
+    //             this.errors = undefined;
+               
+                
+    //         }
+    //         if(error){
+    //             this.errors = error;
+    //             this.records = undefined;
+    //         }
+
+    //     }
+
+}
