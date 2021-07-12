3aa332435cd6bb1b4d2cafbbd40658bf4fce15b0
Kamren <kamren.snell@salesforce.com>
2021-07-12 11:57:33 -0400

    Apex Classes
    Added in the apex classes the pick out tiles based on criteria and finished browseAll component

--- force-app/main/default/lwc/browseAllMovies/browseAllMovies.js
+++ force-app/main/default/lwc/browseAllMovies/browseAllMovies.js
@@ -4,21 +4,39 @@ import getFilteredTitles from '@salesforce/apex/BrowseAllFiltered.getFilteredTit
 export default class BrowseAllMovies extends LightningElement {
     @api titles;
     @track records;
-    @track genre = "";
+    @track genre;
     @track orderBy = "";
 
     handleSelectedGenreEvent(event) {
         this.genre = event.target.value;
         if(this.genre !== "" && this.orderBy !== "") {
-            console.log(this.orderBy);
-            getFilteredTitles(this.genre, this.orderBy);
+            getFilteredTitles({genre: this.genre,
+                               orderBy: this.orderBy
+                })
+                .then(result => {
+                    console.log('Results - ', result[0]);
+                    this.records = result;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     }
 
     handleSelectedOrderEvent(event) {
         this.orderBy = event.target.value;
         if(this.orderBy !== "" && this.genre !== "") {
-            getFilteredTitles(this.genre, this.orderBy);
+            getFilteredTitles({genre: this.genre,
+                               orderBy: this.orderBy
+                })
+                .then(result => {
+                    console.log('Results - ', result[0]);
+                    this.records = result;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
+        console.log(this.records);
     }
 }
