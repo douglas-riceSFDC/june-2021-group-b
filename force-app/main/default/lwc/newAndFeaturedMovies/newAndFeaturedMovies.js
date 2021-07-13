7078d27a4814271edafb99341dbdeadfc80ad263
Kamren <kamren.snell@salesforce.com>
2021-07-13 15:55:40 -0400

    Fixed limit issue

--- force-app/main/default/lwc/newAndFeaturedMovies/newAndFeaturedMovies.js
+++ force-app/main/default/lwc/newAndFeaturedMovies/newAndFeaturedMovies.js
@@ -1,5 +1,19 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import getNewAndFeaturedTitles from '@salesforce/apex/TitleAuraService.getNewAndFeaturedTitles';
 
 export default class NewAndFeaturedMovies extends LightningElement {
-    @api titles;
+    @api limit;
+
+    @track records;
+
+    connectedCallback() {
+        getNewAndFeaturedTitles({ limiter: this.limit})
+            .then(result => {
+                console.log('result: ' + result[0]);
+                this.records = result;
+            })
+            .catch(error => {
+                console.error('Error occured'. error);
+            });
+        }
 }
