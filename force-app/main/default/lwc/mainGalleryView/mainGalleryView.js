6f12289dfe428d35e7635bf1a5a321670df9852b
Kamren <kamren.snell@salesforce.com>
2021-07-13 15:57:02 -0400

    Fixed limit issue

--- force-app/main/default/lwc/mainGalleryView/mainGalleryView.js
+++ force-app/main/default/lwc/mainGalleryView/mainGalleryView.js
@@ -1,17 +1,5 @@
 import { LightningElement, api, track } from 'lwc';
-import getNewAndFeaturedTitles from '@salesforce/apex/TitleAuraService.getNewAndFeaturedTitles';
 
 export default class MainGalleryView extends LightningElement {
     @api limit;
-    @track titles;
-
-    connectedCallback() {
-        getNewAndFeaturedTitles({ limiter: this.limit})
-            .then(result => {
-                this.titles = result;
-            })
-            .catch(error => {
-                console.error('Error occured'. error);
-            });
-    }
 }
