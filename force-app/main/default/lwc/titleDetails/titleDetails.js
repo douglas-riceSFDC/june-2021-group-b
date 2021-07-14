38b56dc163382efd91bc70acf87880d176daa0c0
Donovan Byler <dbyler@salesforce.com>
2021-07-14 13:43:42 -0400

    Added rent functionality to title detail page

--- force-app/main/default/lwc/titleDetails/titleDetails.js
+++ force-app/main/default/lwc/titleDetails/titleDetails.js
@@ -2,12 +2,16 @@ import { LightningElement, track, wire } from "lwc";
 import { CurrentPageReference } from "lightning/navigation";
 //import getExtraFields from "@salesforce/apex/LightningSelfRegisterController.getExtraFields";
 import getTitleById from "@salesforce/apex/TitleAuraService.getTitleById";
+import getAvailableStockForTitle from "@salesforce/apex/StockAuraService.getAllAvailableStockForTitle";
+import rentStock from "@salesforce/apex/StockController.rentStock";
 
 export default class TitleDetails extends LightningElement {
   @track currentPageReference;
   @track urlStateParameters;
   @track title;
-  @track available;
+  @track stock;
+  @track areStockAvailable;
+  @track rentDisabledMessage;
 
   @track titleId;
 
@@ -19,11 +23,12 @@ export default class TitleDetails extends LightningElement {
     }
   }
 
-  setAvailable() {
+  setStockAvailability() {
     if (this.title.Available_Stock__c > 0) {
-      this.available = true;
+      this.areStockAvailable = true;
     } else {
-      this.available = false;
+      this.areStockAvailable = false;
+      this.rentDisabledMessage = "Out of Stock";
     }
   }
 
@@ -31,13 +36,37 @@ export default class TitleDetails extends LightningElement {
     this.titleId = this.urlStateParameters.title;
     getTitleById({ Id: this.titleId })
       .then((result) => {
-        console.log("Results - ", result[0]);
         this.title = result[0];
-        this.setAvailable();
+        this.setStockAvailability();
       })
       .catch((error) => {
         console.log(error);
       });
-    console.log(this.title);
+  }
+
+  handleRental() {
+    getAvailableStockForTitle({ titleId: this.titleId })
+      .then((result) => {
+        this.updateStock(result[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  updateStock(s) {
+    rentStock({ stock: s })
+      .then(() => {
+        this.changeTitleInfoOnPage();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  changeTitleInfoOnPage() {
+    this.areStockAvailable = false;
+    this.rentDisabledMessage = "Movie Succesfully Rented";
+    this.title.Available_Stock__c--;
   }
 }
