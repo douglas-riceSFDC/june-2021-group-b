db78f6a43e7cb52729605b9e7439e8662083e31d
Donovan Byler <dbyler@salesforce.com>
2021-07-14 14:53:35 -0400

    BrowseAll event change to 'onchange'

---
+++ force-app/main/default/lwc/browseAllMovies/browseAllMovies.js
@@ -0,0 +1,41 @@
+import { LightningElement, api, track } from "lwc";
+import getFilteredTitles from "@salesforce/apex/BrowseAllFiltered.getFilteredTitles";
+
+export default class BrowseAllMovies extends LightningElement {
+  @api titles;
+  @track records;
+  @track genre;
+  @track orderBy = "";
+
+  handleSelectedGenreEvent(event) {
+    this.genre = event.target.value;
+    if (this.genre !== "") {
+      if (this.orderBy === "") {
+        this.orderBy = "Release Date";
+      }
+      getFilteredTitles({ genre: this.genre, orderBy: this.orderBy })
+        .then((result) => {
+          console.log("Results - ", result[0]);
+          this.records = result;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
+  handleSelectedOrderEvent(event) {
+    this.orderBy = event.target.value;
+    if (this.orderBy !== "") {
+      getFilteredTitles({ genre: this.genre, orderBy: this.orderBy })
+        .then((result) => {
+          console.log("Results - ", result[0]);
+          this.records = result;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    console.log(this.records);
+  }
+}
