778b60bdf7e8d030e65c513255b57d1f49404374
jcalfeeSFDC <86784593+jcalfeeSFDC@users.noreply.github.com>
2021-07-16 10:58:22 -0400

    Open Movie Database Integration
    Final push for OMDB Integration

--- force-app/main/default/lwc/titleCreateNew/titleCreateNew.js
+++ force-app/main/default/lwc/titleCreateNew/titleCreateNew.js
@@ -1,35 +1,76 @@
+import UserPreferencesShowCityToExternalUsers from '@salesforce/schema/User.UserPreferencesShowCityToExternalUsers';
 import { LightningElement, track, api } from 'lwc';
 
 export default class titleCreateNew extends LightningElement {
     @track titleSearchValue;
     @track stockNumber;
-    @track _movieTitle;
+    @track titleSubmit;
+    @track show = false;
 
-    @api
-    get movietitle(){
-        return this._movieTitle;
-    }
-    set movieTitle(value){
-        this.setAttribute('movieTitle', value);
-        this._movieTitle = value;
-        this.setup();
+    @api movieTitle = '';
+
+
+    handleChange(event){
+        
+        console.log('Value changed!');
+        
+        var selectedImdbId = event.target.value;
+        console.log("This is the selected title: ");
+        console.log("ImdbId: ", selectedImdbId);
+        
+        for (var i = 0; i < this.movieTitle.length; i++){
+            if ( this.movieTitle[i].imdbID == selectedImdbId){
+                this.titleSubmit = this.movieTitle[i];
+                console.log(JSON.stringify(this.titleSubmit));
+            }
+        }
+        
     }
 
-    setup(){
-        console.log("Movie title reassigned.");
-        console.log(this._movieTitle);
+
+    onClickSubmitTitle(event){
+        console.log('Time to submit the title!');
+        
+        const title = this.titleSubmit;
+        const stock = this.stockNumber.toString();
+
+        console.log('The title being submit: ', JSON.stringify( title ) );
+
+        const titleSubmitEvent = new CustomEvent(
+            "submittitle",
+            {
+                detail: 
+                {
+                    "title": title,
+                    "stock": stock
+                }
+            }
+        );
+
+        // Clear the variables!
+        this.movieTitle = '';
+        this.titleSubmit = '';
+        this.stock = '';
+        this.show = false;
+        this.template.querySelector('lightning-input[data-name="titleSearch"]').value = '';
+        this.template.querySelector('lightning-input[data-name="stockAmount"]').value = '';
+
+        this.dispatchEvent(titleSubmitEvent);
     }
+
 
     onClickRetrieveTitles(event){
+        console.log("Retrieve request recieved!");
+
         this.titleSearchValue   = this.template.querySelector('lightning-input[data-name="titleSearch"]').value;
         this.stockNumber        = this.template.querySelector('lightning-input[data-name="stockAmount"]').value;
+        this.show               = true;
 
         const titleName = this.titleSearchValue;
 
         console.log("Values entered:");
         console.log("Title search value: ",     titleName);
         console.log("Stock amount value: ",     this.stockNumber);
-        console.log("movieTitle value: ",       this.movieTitle);
 
         const valueChangeEvent = new CustomEvent(
             "valuechange",
